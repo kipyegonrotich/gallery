7839cbc8ab971ce14b785234436b4fb96e196b28
Nix <kipyegonrotich@gmail.com>
2025-06-21 14:24:46 +0300

    credentials added

--- _config.js
+++ _config.js
@@ -1,15 +1,9 @@
-var config = {}
+const config = {};
 
-// Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
-    production: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-=======
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> test
-}
+  production: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+  development: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  test: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+};
+
 module.exports = config;
