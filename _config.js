b63afbd7446572752941dbaa74cacf904071c9ed
Nix <kipyegonrotich@gmail.com>
2025-06-21 18:37:22 +0300

    Corrected cluster

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
 const config = {};
 
 config.mongoURI = {
-  production: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-  development: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-  test: 'mongodb+srv://<nixone>:<MyOneninE>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+  production: 'mongodb+srv://nixone:MyOneninE@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+  development: 'mongodb+srv://nixone:MyOneninE@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  test: 'mongodb+srv://nixone:MyOneninE@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 };
 
 module.exports = config;
