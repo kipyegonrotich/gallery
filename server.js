47fc125639c62a3bbc684384353ff9442159bfe8
Nix <kipyegonrotich@gmail.com>
2025-06-25 19:38:10 +0300

    Dependencies updated

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Dependencies
+// Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -9,35 +9,43 @@ const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const image = require('./routes/image');
 
+// Initialize app
 const app = express();
 
-// Database connection
+// Database connection 
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},(err)=>{
     if (err) {
         console.log(err)
     }else{
         console.log(`Connected to Database: ${MONGODB_URI}`)
     }
 });
+// test if the database has connected successfully
 
-// View Engine
+
+// View engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
+// Body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Handles form submissions
+
+// Routes
 app.use('/', index);
 app.use('/image', image);
 
-// Render configuration 
+// Render port configuration
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () =>{
-    console.log(`Server is listening at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server is listening at http://localhost:${PORT}`);
 });
-
 
 module.exports = app;
