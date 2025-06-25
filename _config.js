98433ac4826cd13b972facbff42aeb55a6a0deae
Nix <kipyegonrotich@gmail.com>
2025-06-25 18:24:51 +0300

    change password

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 const config = {};
 
+// MongoDB Atlas SRV connection strings
 config.mongoURI = {
-  production: 'mongodb+srv://nixone:MyOneninE@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-  development: 'mongodb+srv://nixone:MyOneninE@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-  test: 'mongodb+srv://nixone:MyOneninE@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+  production: 'mongodb+srv://nixone:MyOneninE@nixipone.mongodb.net/darkroom?retryWrites=true&w=majority',
+  development: 'mongodb+srv://nixone:MyOneninE@nixipone.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  test: 'mongodb+srv://nixone:MyOneninE@nixipone.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 };
 
 module.exports = config;
